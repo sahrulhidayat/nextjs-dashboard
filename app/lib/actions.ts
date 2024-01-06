079bce96177b78de0a3d30a2b4ca8c148cbe1ada
Sahrul Hidayat <sahrulhint@gmail.com>
2024-01-06 08:35:11 +0700

    Complete the learning path

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
     id: z.string(),
@@ -12,7 +14,7 @@ const FormSchema = z.object({
     }),
     amount: z.coerce
         .number()
-        .gt(0, { message: 'Please enter an amount greater than $0.'}),
+        .gt(0, { message: 'Please enter an amount greater than $0.' }),
     status: z.enum(['pending', 'paid'], {
         invalid_type_error: 'Please select an invoice status.',
     }),
@@ -71,7 +73,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
 
 export async function updateInvoice(
     id: string,
-    prevState: State, 
+    prevState: State,
     formData: FormData
 ) {
     const validatedFields = UpdateInvoice.safeParse({
@@ -84,7 +86,7 @@ export async function updateInvoice(
         return {
             errors: validatedFields.error.flatten().fieldErrors,
             message: 'Missing Fields. Failed to Update Invoice.',
-          };
+        };
     }
 
     const { customerId, amount, status } = validatedFields.data;
@@ -111,5 +113,24 @@ export async function deleteInvoice(id: string) {
         return { message: 'Deleted Invoice.' };
     } catch (error) {
         return { message: 'Database Error: Failed to Delete Invoice.' };
+    }
+}
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
     }
 }
